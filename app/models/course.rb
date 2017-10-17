5950278c9254add6db7ec466eea233e6802bb6b1
ahmedhossammontasser <a.hossam.2010@gmail.com>
2017-10-17 12:54:10 +0200

    Added missing column in user_role table, updated has many in course , group model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,17 @@
 class Course < ApplicationRecord
 	belongs_to :user
-	has_many :groups, :order => "position", :dependent => :destroy
-	has_many :lectures, :dependent => :destroy
-	has_many :quizzes,:order => :position, :dependent => :destroy 	
-	has_many :custom_links, :dependent => :destroy
+	has_many :groups, -> { order('position') }, :dependent => :destroy
+	has_many :lectures, -> { order('position') }, :dependent => :destroy
+	has_many :quizzes, -> { order('position') }, :dependent => :destroy 	
+	has_many :custom_links, -> { order('position') }, :dependent => :destroy
 	has_many :announcements, :dependent => :destroy
 	
 	has_many :enrollments, :dependent => :destroy
-	has_many :users, :through => :enrollments, :uniq => true
+	has_many :users, -> { distinct }, :through => :enrollments
 
 	has_many :teacher_enrollments, :dependent => :destroy
-	has_many :teachers, :source => :user, :through => :teacher_enrollments, :uniq => true
+	has_many :teachers, -> { distinct }, :source => :user, :through => :teacher_enrollments
 
 	has_many :guest_enrollments, :dependent => :destroy
-	has_many :guests, :source => :user, :through => :guest_enrollments, :uniq => true
+	has_many :guests, -> { distinct }, :source => :user, :through => :guest_enrollments
 end
