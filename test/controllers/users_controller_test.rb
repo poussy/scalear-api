3970dab08e2117304bada2a398dfbd27113b61e6
Okasha <okashoon@gmail.com>
2017-10-22 13:21:36 +0200

    add devise_token_auth gem, create migration for user table and create users controller

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  # test 'signing in' do
+    # user = {password: "aa123456", email: "okashoon@gmail.com"}
+    # user.sign_in
+  # end
+end
