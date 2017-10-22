bfb94c86fc4674103ee32957e906bc25670b063a
Okasha <okashoon@gmail.com>
2017-10-22 15:55:16 +0200

    make migrations for user columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022092133) do
+ActiveRecord::Schema.define(version: 20171022115722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20171022092133) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "screen_name"
+    t.string "university"
+    t.string "link"
+    t.string "bio"
+    t.integer "discussion_pref", default: 1
+    t.text "completion_wizard"
+    t.integer "first_day", default: 0
+    t.integer "canvas_id"
+    t.datetime "canvas_last_signin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
