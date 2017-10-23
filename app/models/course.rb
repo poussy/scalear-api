7789bdfaa9b580d394278406e2c1580e5c8ad52c
ahmedhossammontasser <a.hossam.2010@gmail.com>
2017-10-23 11:25:27 +0200

    Finished SR-5 , SR-37 , SR-104
    SR-5 Create course model and controller
    SR-37 A course must have a unique enrollment key
    SR-104 A course must have a guest enrollment key

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-	belongs_to :user
+	# belongs_to :user
 	has_many :groups, -> { order('position') }, :dependent => :destroy
 	has_many :lectures, -> { order('position') }, :dependent => :destroy
 	has_many :quizzes, -> { order('position') }, :dependent => :destroy 	
@@ -14,4 +14,58 @@ class Course < ApplicationRecord
 
 	has_many :guest_enrollments, :dependent => :destroy
 	has_many :guests, -> { distinct }, :source => :user, :through => :guest_enrollments
+
+	validates :name, :end_date, :short_name,:start_date, :user_id, :time_zone, :presence => true
+
+	before_create :create_unique_identifier
+	before_create :create_guest_unique_identifier
+
+	# validates :unique_identifier, :guest_unique_identifier, uniqueness: true
+	validate :unique_identifier_guest_unique_identifier_not_changed
+
+	validate :validate_end_date_disable_regis_after_start_date ,on: [:create, :update]
+	validates_format_of :image_url, :with    => %r{\.(((g|G)(i|I)(f|F))|((j|J)(p|P)(e|E)?(g|G))|((p|P)(n|N)(g|G)))}i, :message => :must_be_image, :allow_blank => true
+
+
+	accepts_nested_attributes_for :users, :allow_destroy => true
+	accepts_nested_attributes_for :groups, :allow_destroy => true
+
+
+
+	
+	private 
+		def validate_end_date_disable_regis_after_start_date
+			if end_date && start_date && end_date < start_date
+				errors.add(:end_date, "courses.errors.end_date_pass")
+			end
+			if disable_registration && disable_registration < start_date
+				errors.add(:disable_registration, "courses.errors.end_date_pass")
+			end
+		end
+
+		def unique_identifier_guest_unique_identifier_not_changed
+			if unique_identifier_changed?  && self.persisted?
+				errors.add(:unique_identifier, "courses.errors.unique_identifier_changed")
+			end
+			if guest_unique_identifier_changed? && self.persisted?
+				errors.add(:guest_unique_identifier, "courses.errors.guest_unique_identifier_changed")
+			end
+		end
+
+		def create_unique_identifier
+			begin
+				self.unique_identifier = generate_random_unique_identifier
+			end while ( self.class.exists?(:unique_identifier => unique_identifier)  || (self.class.exists?(:guest_unique_identifier => unique_identifier)  ) )
+	 	end
+
+		def create_guest_unique_identifier
+			begin
+				self.guest_unique_identifier = generate_random_unique_identifier
+			end while  ( self.class.exists?(:unique_identifier => guest_unique_identifier)  || (self.class.exists?(:guest_unique_identifier => guest_unique_identifier)  ) )
+		end
+
+		def generate_random_unique_identifier
+		(0...5).map { [*('A'..'H'),*('J'..'N'), *('P'..'Z')].to_a[rand(24)] }.join + '-' + (0...5).map { (0..9).to_a[rand(10)] }.join
+		end
+
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,86 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	# def setup
+	   # @c = courses(:one)
+	 # end
+	
+	test "invalid without a name" do
+	course = Course.new
+		assert_not course.valid?
+		assert_equal [:name, :end_date, :short_name, :start_date, :user_id, :time_zone], course.errors.keys
+		course.name = 'name'
+		course.short_name = 'short_name'
+		course.user_id = 1
+		course.time_zone = "time_zone"
+		course.start_date = 'time_zone'
+		course.end_date = 'time_zone'
+		assert_not course.valid?
+		assert_equal [ :end_date,  :start_date], course.errors.keys
+
+		course.start_date = '2017-9-9'.to_datetime
+		course.end_date = '2017-8-9'.to_datetime
+		assert_not course.valid?
+
+		course.start_date = '2017-9-9'.to_datetime
+		course.end_date = '2017-10-9'.to_datetime
+		assert course.valid?	   
+
+		course.disable_registration = '2017-8-9'.to_datetime
+		assert_not course.valid?
+		assert_equal [:disable_registration], course.errors.keys
+
+		course.disable_registration = '2017-9-9'.to_datetime
+		assert course.valid?
+
+		course.image_url = '2017-8-9'.to_datetime
+		assert_not course.valid?
+		assert_equal [ :image_url], course.errors.keys
+
+		course.image_url = 'https://www.facebook.com/'
+		assert_not course.valid?
+		assert_equal [ :image_url], course.errors.keys
+
+		course.image_url = 'https://pbs.twimg.com/profile_images/839721704163155970/LI_TRk1z_400x400.jpg'
+
+		course.save	   
+		assert course.valid?
+
+
+		## Validate A course can not change unique_identifier or guest_unique_identifier
+		course2 = Course.new
+		course2.name = 'name'
+		course2.short_name = 'short_name'
+		course2.user_id = 1
+		course2.time_zone = "time_zone"
+		course2.start_date = '2017-9-9'.to_datetime
+		course2.end_date = '2017-10-9'.to_datetime
+		assert course2.valid?	   
+		course2.image_url = 'https://pbs.twimg.com/profile_images/839721704163155970/LI_TRk1z_400x400.jpg'
+		assert course2.valid?
+		course2.save	   
+
+		course2.unique_identifier = course.unique_identifier
+		assert_not course2.valid?
+		assert_equal [ :unique_identifier], course2.errors.keys
+
+
+		## Validate A course must have a guest enrollment key
+		## validate A course must have a unique enrollment key
+		course3 = Course.new
+		course3.name = 'name'
+		course3.short_name = 'short_name'
+		course3.user_id = 1
+		course3.time_zone = "time_zone"
+		course3.start_date = '2017-9-9'.to_datetime
+		course3.end_date = '2017-10-9'.to_datetime
+		assert course3.valid?	   
+		course3.image_url = 'https://pbs.twimg.com/profile_images/839721704163155970/LI_TRk1z_400x400.jpg'
+		assert course3.valid?
+		course3.unique_identifier = course2.guest_unique_identifier
+		course3.guest_unique_identifier = course2.guest_unique_identifier
+		assert course3.valid?
+		course3.save	   
+
+	 end
 end
