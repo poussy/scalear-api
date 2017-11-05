42f788ea70415b1857b095a6f97c5c66ab89c832
Ahmed Okasha <okashoon@gmail.com>
2017-11-05 19:48:42 +0200

    create unit test for quizzes_controller

--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class QuizzesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    ## create user for authorization, and set this user to be a teacher in course3, below tests wil use items in course3
+    @user = users(:user3)
+
+		@user.roles << Role.find(1)
+	
+		@course = courses(:course3)
+
+		@course.teacher_enrollments.create(:user_id => 3, :role_id => 1, :email_discussion => false)
+    
+  end
+
+  test "should create quiz" do
+
+    assert_equal Quiz.count, 1
+
+		post '/en/courses/3/quizzes/new_or_edit/', params:{:group => 3, :type => 'quiz'},headers: @user.create_new_auth_token
+
+    assert_equal Quiz.count, 2
+    
+		
+	end
+
+
 end
