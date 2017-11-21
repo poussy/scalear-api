50f6cb3fb98548a2de1ae5c9385f625bd245c79b
Ahmed Okasha <okashoon@gmail.com>
2017-11-21 12:58:01 +0200

    - add review module timeline -add functionalities to Time class in application_controller

--- app/models/video_event.rb
+++ app/models/video_event.rb
@@ -16,4 +16,14 @@ class VideoEvent < ApplicationRecord
 
 	# def self.get_event(event)
 	# end
+
+	def self.get_rounded_time_module(array)
+		return_hash={}
+		Time.zone="UTC"
+		array.each do |k,v|
+			parsed_time=Time.zone.parse(Time.seconds_to_time(k[1])).floor(15.seconds).to_i #currently rounding to nearest minute. could change that. #to_i to use it in javascript
+			return_hash[(parsed_time+k[0]).to_i] = (return_hash[(parsed_time+k[0]).to_i]||0) + 1
+		end
+		return return_hash.to_a    
+  	end
 end
