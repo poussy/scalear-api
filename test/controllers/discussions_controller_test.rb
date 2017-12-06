8bd4905d6a978d53fe544a8d435f7181b1420498
ahmedhossammontasser <a.hossam.2010@gmail.com>
2017-12-06 16:04:14 +0200

    Removed  test  case from discussions controller

--- test/controllers/discussions_controller_test.rb
+++ test/controllers/discussions_controller_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class DiscussionsControllerTest < ActionDispatch::IntegrationTest
 
 	# def create_post
@@ -11,24 +10,5 @@ class DiscussionsControllerTest < ActionDispatch::IntegrationTest
 	# def flag
 	# def vote_comment
 	# def flag_comment
-
-	test 'validate index method for Admin' do
-		url = '/en/discussion'
-		get  url ,headers: @admin_user.create_new_auth_token 
-		resp =  JSON.parse response.body
-		assert_equal resp['total'] , 5
-	end		
-
-	# test 'validate new method for teacher' do
-	# 	url = '/en/courses/new'
-	# 	get  url ,headers: @user1.create_new_auth_token 
-	# 	resp =  JSON.parse response.body
-	# 	assert_equal resp['importing'].count , 1
-		
-	# 	@course2.add_professor(@user1,false)
-	# 	get  url ,headers: @user1.create_new_auth_token 
-	# 	resp =  JSON.parse response.body
-	# 	assert_equal resp['importing'].count , 2
-	# end	
-
+	
 end
