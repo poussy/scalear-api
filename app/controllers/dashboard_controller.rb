d2bc3b54d50d4fe88b4769f273385b8eb9e3a2b0
Ahmed Okasha <okashoon@gmail.com>
2018-01-08 15:11:26 +0200

    add dynamic_url to dashboard_cont

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -93,7 +93,63 @@ class DashboardController < ApplicationController
 		render json: {events: final_events, key:key , module_summary_id_list:module_summary_id_list}
 	end
 
-	# def dynamic_url
-	# end
+	def dynamic_url
+			key = params[:key].tr(" ","+")
+			id =  AESCrypt.decrypt(key,ENV["user_ase_key"])
+			puts id
+			current_user = User.find(id)
+			puts current_user
+			user = User.where(:id => current_user.id).includes({:online_quiz_grades => [:online_quiz, :lecture]}, {:free_online_quiz_grades => [:online_quiz , :lecture] }, {:lecture_views => :lecture }, :assignment_item_statuses, {:quiz_statuses => :quiz})[0]
+			teacher_events = []
+			student_events = []
+			if user.is_administrator?
+					teacher_courses = Course.pluck(:id)
+					student_courses = []
+			elsif current_user.is_school_administrator?
+					email = current_user.email.split('@')[1]
+					teacher_courses = Course.includes([:user,:teachers]).all.select{|c| c.teachers.map{|e| e.email.split("@")[1]}.include?(email) }.map { |e| e.id }
+					student_courses = []
+			else
+					teacher_courses = user.subjects_to_teach.pluck("courses.id")
+					student_courses = user.courses.pluck("courses.id")
+			end
+
+			today = Time.now
+			filter = lambda{|ev|
+					(ev.start_at < today+100.years) &&
+					(!ev.lecture_id && !ev.quiz_id && ev.group.appearance_time <= today) ||
+					(ev.lecture_id && ev.lecture.appearance_time <= today) ||
+					(ev.quiz_id && ev.quiz.appearance_time <= today)
+			}
+
+			events_all = []
+			events_all << Event.where(:course_id => teacher_courses).includes([:course, :group, :lecture, :quiz]).select(&filter) if teacher_courses.size > 0
+			events_all << Event.where(:course_id => student_courses).includes([:course, {:group => [:lectures, {:online_quizzes => [:lecture, :online_answers]}, :quizzes]}, {:lecture =>[:online_quizzes, :online_quiz_grades, :free_online_quiz_grades]}, :quiz]).select(&filter) if student_courses.size > 0
+
+			time_zone = 'Etc/UTC'
+			if TZInfo::Timezone.all_identifiers.include?((params[:tz]))
+					time_zone = params[:tz]
+			end
+
+			calendar_event = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\n"
+			calendar_event += "BEGIN:VTIMEZONE\r\n"
+			calendar_event += "TZID:"+time_zone+"\r\n"
+			calendar_event += "END:VTIMEZONE\r\n"
+			events_all.each do |course_events|
+					course_events.each do |event|
+							start_at_time_zone = event.start_at.in_time_zone(time_zone)
+							calendar_event += "BEGIN:VEVENT\r\nCLASS:PUBLIC\r\n"
+							calendar_event += "DESCRIPTION:"+event.course.short_name+": "+event.name.split(" due")[0]+" "+I18n.t("controller_msg.due").capitalize+" "+I18n.t("at")+" "+start_at_time_zone.strftime("%H:%M")+" "+root_url.split("en")[0]+"#/courses/"+event.course_id.to_s+"\r\n" #/modules/2308/progress\r\n"
+							calendar_event += "DTSTART:"+start_at_time_zone.strftime("%Y%m%d")+"T"+start_at_time_zone.strftime("%H%M%S")+"\r\n"
+							calendar_event += "DTEND:"+start_at_time_zone.strftime("%Y%m%d")+"T"+start_at_time_zone.strftime("%H%M%S")+"\r\n"
+							calendar_event += "LOCATION:Scalable-Learning\r\n"
+							calendar_event += "SUMMARY;LANGUAGE=en-us:"+event.course.short_name+": "+event.name.split(" due")[0]+"\r\n"
+							calendar_event += "TRANSP:TRANSPARENT\r\n"
+							calendar_event += "END:VEVENT\r\n"
+					end
+			end
+			calendar_event += "END:VCALENDAR"
+			render :json => calendar_event.strip
+	end
 
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -39,7 +39,7 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     assert_equal decode_json_response_body['events'].size, 1
     event1=decode_json_response_body['events'][0]
     assert_equal event1['title'], 'New Module due'
-    assert_equal Time.parse(event1['start']).to_i/10.floor, (Time.now+1.days).to_i/10.floor 
+    assert_equal Time.parse(event1['start']).to_i/100.floor, (Time.now+1.days).to_i/100.floor 
     assert_equal event1['color'], 'gray'
     assert_equal event1['textColor'], 'white'
     assert_equal event1['status'], -1
@@ -57,14 +57,24 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     get '/en/dashboard/get_dashboard', headers: @admin.create_new_auth_token
 
     assert_equal decode_json_response_body['events'].size, 7
-    # get '/en/dashboard/get_dashboard', headers: @school_admin.create_new_auth_token
-    # pp decode_json_response_body
   end
 
-   test "get_dashboard for school_admin" do
+  test "get_dashboard for school_admin" do
 
     get '/en/dashboard/get_dashboard', headers: @school_admin.create_new_auth_token
     assert decode_json_response_body['events'].size, 1
     
+  end
+
+  test "dynamic_url" do
+
+    get '/en/dashboard/dynamic_url', params:{tz:"Africa",key:"rzmk3chbkS3K0QxERP3QFg=="}, headers: @student.create_new_auth_token #key for id 6
+    assert response.body.include? "DESCRIPTION:c3: New Module"
+    assert response.body.include? "DTSTART:20170909T000000\r"
+    assert response.body.include? "DTEND:20170909T000000\r"
+    assert response.body.include? "DTSTART:20170909T000000\r"
+    assert response.body.include? "DTEND:20170909T000000\r"
+    assert response.body.include? "LOCATION:Scalable-Learning\r"
+  
   end
 end
