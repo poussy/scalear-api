46e45ff18e4b67e4c2d8b31acc0092cc266d9541
Ahmed Okasha <okashoon@gmail.com>
2018-01-17 15:39:30 +0200

    remove logs

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -3,7 +3,6 @@ class Devise::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
    def create
      if params[:email] && ScalearApi::Application.config.domain_account_block.select{|t| params[:email].include?(t)}.size>0
-     pp params[:email]
        render json: {:errors => {:email => [I18n.t("can_not_use_domain")] , :school_provider=> 'true'}} ,:status => :unprocessable_entity
      else
        render_create_success
