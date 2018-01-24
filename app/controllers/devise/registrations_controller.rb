441774c3a51fc640ca1d1975bd955456a8d8531d
Ahmed Okasha <okashoon@gmail.com>
2018-01-24 12:15:46 +0200

    fix devise registration

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -5,7 +5,7 @@ class Devise::RegistrationsController < DeviseTokenAuth::RegistrationsController
      if params[:email] && ScalearApi::Application.config.domain_account_block.select{|t| params[:email].include?(t)}.size>0
        render json: {:errors => {:email => [I18n.t("can_not_use_domain")] , :school_provider=> 'true'}} ,:status => :unprocessable_entity
      else
-       render_create_success
+       super
      end
    end
 
