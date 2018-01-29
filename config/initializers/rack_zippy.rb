a2c5123911d6da9dfea9951c10d9764d7f079dfa
Karim Alaa <karim.hamed89@gmail.com>
2018-01-29 12:22:19 +0200

    removed unneeded configurations

--- config/initializers/rack_zippy.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.insert_before(ActionDispatch::Static, Rack::Zippy::AssetServer)
-
-Rack::Zippy.configure do |config|
-  config.static_extensions.delete('html')
-end
