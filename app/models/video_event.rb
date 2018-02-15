8c3a050202f49a05cd0f274ca56bab99e53e75e6
Ahmed Okasha <okashoon@gmail.com>
2018-02-15 16:47:15 +0200

    add new get_questions_rounded_time_module for discussion questions in groups ctrl

--- app/models/video_event.rb
+++ app/models/video_event.rb
@@ -29,4 +29,21 @@ class VideoEvent < ApplicationRecord
 		end
 		return return_hash.to_a    
   	end
+
+	def self.get_questions_rounded_time_module(array)
+		return_hash={}
+		questions={}
+		Time.zone="UTC"
+		array.each do |k,v|
+			parsed_time=Time.zone.parse(Time.seconds_to_time(k[1])).floor(15.seconds).to_i #currently rounding to nearest minute. could change that. #to_i to use it in javascript
+			return_hash[(parsed_time+k[0]).to_i] = (return_hash[(parsed_time+k[0]).to_i]||0) + 1
+			if questions[(parsed_time+k[0]).to_i]
+			questions[(parsed_time+k[0]).to_i] << v[1]
+			else
+			questions[(parsed_time+k[0]).to_i] = [v[1]]
+			end
+		end
+		return_hash.merge!(questions){|k,v1,v2| [v1,v2]}
+		return return_hash
+  	end
 end
