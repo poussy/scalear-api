9e1666bd207fe823fb6ae357fe2dd0fc35bfcabc
Ahmed Okasha <okashoon@gmail.com>
2018-02-28 16:06:22 +0200

    move user anonymisation and deanonymisation code to user model, and add migration script to fill policy_agreement for users

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -9,22 +9,9 @@ class Devise::SessionsController < DeviseTokenAuth::SessionsController
         user = User.find_by_encrypted_email(encrypted_email)
         if !user.nil? &&  user.valid_password?(resource_params[:password])
             
-            user.email = resource_params[:email]
-            ## use user's email to decrypt information
-            key   = ActiveSupport::KeyGenerator.new(email).generate_key(ENV['gdpr_salt'],32)
-            crypt = ActiveSupport::MessageEncryptor.new(key)
-            
-            user.name =crypt.decrypt_and_verify(user.encrypted_data['name'])
-            user.last_name = crypt.decrypt_and_verify(user.encrypted_data['last_name'])
-            user.screen_name =crypt.decrypt_and_verify(user.encrypted_data['screen_name'])
-            user.university =crypt.decrypt_and_verify(user.encrypted_data['university'])
-            user.encrypted_email = nil
-            user.encrypted_data = nil
-            user.skip_confirmation!
-            user.skip_reconfirmation!
             
             # create token and sign in user
-            @resource = user
+            @resource = user.deanonymise(email)
             
             @client_id = SecureRandom.urlsafe_base64(nil, false)
             @token     = SecureRandom.urlsafe_base64(nil, false)
@@ -32,9 +19,11 @@ class Devise::SessionsController < DeviseTokenAuth::SessionsController
             token: BCrypt::Password.create(@token),
             expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
             }
-            @resource.save
-
-            render_create_success
+            if @resource.save
+                render_create_success
+            else 
+                render json: @resource.errors
+            end
         else
 
             render json: {
--- db/migrate/20180221150201_add_policy_agreement_to_users.rb
+++ db/migrate/20180221150201_add_policy_agreement_to_users.rb
@@ -1,5 +1,11 @@
 class AddPolicyAgreementToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :policy_agreement, :json
+
+    ActiveRecord::Base.transaction do
+      User.where(policy_agreement:nil).each do |user|
+        user.update_attribute('policy_agreement',{'date' => user.created_at, 'ip' => '0.0.0.0'})
+      end
+    end
   end
 end
