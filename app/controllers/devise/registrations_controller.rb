308ca84349ae62e4fcfcf8ba832f422f6824a962
Ahmed Okasha <okashoon@gmail.com>
2018-02-28 12:37:31 +0200

    check for duplicate pseudoanonymised email when creating new user

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -2,8 +2,14 @@ class Devise::RegistrationsController < DeviseTokenAuth::RegistrationsController
   before_action :set_user_by_token
 
   def create
+    email = sign_up_params[:email]
+    encrypted_email = Digest::SHA256.hexdigest (email)
+    
     if params[:email] && ScalearApi::Application.config.domain_account_block.select{|t| params[:email].include?(t)}.size>0
       render json: {:errors => {:email => [I18n.t("can_not_use_domain")] , :school_provider=> 'true'}} ,:status => :unprocessable_entity
+    ##check if a pseudoanonimised user has same email
+    elsif !User.find_by_encrypted_email(encrypted_email).nil?
+      render json: {:errors => {:email => ["has already been taken"]}} ,:status => :unprocessable_entity
     else
       super
     end
