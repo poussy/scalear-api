f10df404a10235f5c16c1cf483e02e9d8e1f36ef
Ahmed Okasha <okashoon@gmail.com>
2018-03-21 19:11:42 +0200

    add validate_user actoin to users_ctrl & change saml_signup to send user data and not redirect & fix agree_to_privacy_policy action

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -1,12 +1,6 @@
 class Devise::ConfirmationsController < DeviseTokenAuth::ConfirmationsController
     def create
         self.resource = resource_class.send_confirmation_instructions(params)
-       
-        if !successfully_sent?(resource)
-            resource.send_confirmation_instructions
-        else
-            respond_with(resource)
-        end
     end
 
     def show
