8f895daf2bf0314dc4528af03bf7fc3099b92c3b
Ahmed Okasha <okashoon@gmail.com>
2018-03-29 14:47:30 +0200

    fix action_mailer.default_url_options, and config.frontend_host variables for staing2, test and production

--- config/environments/staging2.rb
+++ config/environments/staging2.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'scalear-staging2.herokuapp.com/#' , :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'scalear-staging2.herokuapp.com' , :protocol => 'https'}
   config.action_mailer.asset_host = 'https://scalear-staging2.herokuapp.com'
 
 
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   config.active_record.migration_error = :page_load
 
-  config.frontend_host = "http://scalear-staging2.herokuapp/#/"
+  config.frontend_host = "http://scalear-staging2.herokuapp.com/#/"
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
