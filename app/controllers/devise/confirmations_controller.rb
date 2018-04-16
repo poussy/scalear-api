80b873c33ff751bd20922e2459d02cbc3749a816
Okasha <okasha@novelari.com>
2018-04-16 14:27:19 +0200

    fix create action in devise::confirmationsController to check first if user exists or not

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -1,11 +1,13 @@
 class Devise::ConfirmationsController < DeviseTokenAuth::ConfirmationsController
     def create
-        self.resource = resource_class.send_confirmation_instructions(params)
-       
-        if !successfully_sent?(resource)
-            resource.send_confirmation_instructions
+        resource = User.where(email:params[:email])[0]
+        if resource.blank?
+            render json: {errors: {email:["User doesn't exist"]}}, :status => :unprocessable_entity
         else
-            respond_with(resource)
+            resource.send_confirmation_instructions({
+                client_config: params[:config_name],
+                redirect_url: params[:confirm_success_url]
+              })
         end
     end
 
