ad73b143dddb354044c02c209d1f43a06ff631ac
poussy <poussy@novelari.com>
2019-01-21 13:54:58 +0200

    saml user last sign in becomes , current sing in

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -97,7 +97,7 @@ class SamlController < ApplicationController
           saml_user.skip_confirmation!
           saml_user.save
         end
-        saml_user.last_sign_in_at = Time.now
+        saml_user.current_sign_in_at = Time.now
         token = saml_user.create_new_auth_token
         return "#/users/login?#{token.to_query}"
       end
