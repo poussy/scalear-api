e538205338962b7e563ed584139a2413681132b3
poussy <poussy@novelari.com>
2020-01-27 22:14:15 +0200

    export modules,lectures,quizzes and in video quizzes , yet surveys to do

--- app/modules/cc_utils.rb
+++ app/modules/cc_utils.rb
@@ -1,12 +1,9 @@
 module CcUtils
+    
     def cc_course(course)
-        transformed_course = CanvasCc::CanvasCC::Models::Course.new
-        transformed_course.title = course.name
-        transformed_course.grading_standards = []
-        transformed_course.identifier = course.id.to_s
-        transformed_course.grading_standards=[]
+        transformed_course = create_transformed_course(course)
         course.groups.each do |group|
-            transformed_group = cc_groups(group)
+            transformed_group = cc_groups(group,transformed_course)
             transformed_course.canvas_modules << transformed_group
         end 
         dir = Dir.mktmpdir
@@ -14,25 +11,223 @@ module CcUtils
         path = carttridge.create(dir)
         return path   
     end
-
-    def cc_groups(group)
-        transformed_group = CanvasCc::CanvasCC::Models::CanvasModule.new
-        transformed_group.title = group.name
-        transformed_group.identifier = group.id
-
-        group.lectures.each do |lecture|
-            transformed_lecture = cc_items(lecture)
-            transformed_group.module_items << transformed_lecture
-        end    
+    def cc_groups(group,transformed_course)
+        transformed_group = create_transformed_group(group)
+        attach_lectures(group,transformed_group)
+        attach_in_video_quizzes(group,transformed_group,transformed_course)
+        attach_quizzes(group,transformed_group,transformed_course)
         return transformed_group
     end 
+ 
+    def cc_lecture_items(lecture)
+        transformed_lecture = create_transformed_lecture(lecture)
+        return transformed_lecture
+    end
+    def cc_quiz_items(quiz)
+        transformed_quiz = create_transformed_assessment(quiz)
+        transformed_quiz.title = quiz.name
+        transformed_quiz.due_at = quiz.due_date
+        transformed_quiz.allowed_attempts = quiz.retries
+        attach_questions(quiz,transformed_quiz)
+        return transformed_quiz
+    end
+    def cc_quiz_question(quiz)
+        transformed_question_type = map_SL_quiz_type_to_CC_question_type(quiz.question_type,quiz) 
+        transformed_question = create_transformed_question(transformed_question_type)
+        transformed_question.material = extract_inner_html_text(quiz.content)
+        if transformed_question_type != "essay_question"
+            quiz.answers.each do |answer|
+                transformed_answer = cc_quiz_answer(answer,transformed_question)
+                transformed_question.answers << transformed_answer
+            end    
+        end
+        return transformed_question
+    end
+    def cc_quiz_answer(answer,transformed_question)
+        transformed_answer = create_transformed_answer(answer)
+        if answer.correct
+            transformed_answer.fraction=1.0
+        else
+            transformed_answer.fraction=0.0
+        end         
+        if transformed_question.question_type === 'fill_in_multiple_blanks_question'
+            transformed_answer.resp_ident = 'answer'+transformed_answer.id.to_s
+            transformed_question.material += " [#{transformed_answer.resp_ident}]"
+        end    
+        return transformed_answer
+    end   
 
-    def cc_items(lecture)
-        transformed_lecture = CanvasCc::CanvasCC::Models::ModuleItem.new
-        transformed_lecture.identifier = lecture.id
+    def attach_lectures(group,transformed_group)
+        group.lectures.each do |lecture|     
+            transformed_lecture = cc_lecture_items(lecture)
+            transformed_group.module_items << transformed_lecture
+        end    
+    end    
+    def attach_in_video_quizzes(group,transformed_group,transformed_course)       
+        group.lectures.each do |lecture|
+            in_video_assessment = create_transformed_assessment(lecture)
+            in_video_assessment.title = lecture.name+" quizzes"
+            lecture.online_quizzes.each_with_index do |in_video_quiz,i|
+                in_video_question = create_transformed_question('text_only_question')
+                in_video_question.title = 'question'
+                in_video_question.material = format_in_video_quiz_body(in_video_quiz,lecture.url)
+                in_video_assessment.items << in_video_question
+            end            
+            in_video_module_item = create_module_item("Quizzes::Quiz",in_video_assessment.identifier)
+            transformed_group.module_items << in_video_module_item
+            transformed_course.assessments<<in_video_assessment
+        end          
+    end    
+    def attach_quizzes(group,transformed_group,transformed_course)
+        group.quizzes.each do |quiz|
+
+            transformed_quiz = cc_quiz_items(quiz)
+
+            # quiz_module_item = CanvasCc::CanvasCC::Models::ModuleItem.new
+            # quiz_module_item.content_type='Quizzes::Quiz' 
+            # quiz_module_item.title = 'in_video_module_item'   
+            # quiz_module_item.identifier = CanvasCc::CC::CCHelper.create_key(quiz_module_item)
+            # quiz_module_item.identifierref = transformed_quiz.identifier
+            quiz_module_item = create_module_item('Quizzes::Quiz' ,transformed_quiz.identifier)
+            transformed_group.module_items << quiz_module_item
+            transformed_course.assessments << transformed_quiz
+        end    
+    end   
+    def attach_questions(quiz,transformed_quiz)
+        quiz.questions.each do |q|
+            transformed_question = cc_quiz_question(q)
+            transformed_quiz.items << transformed_question
+        end     
+    end    
+
+
+    def create_transformed_course(course)
+        transformed_course = CanvasCc::CanvasCC::Models::Course.new
+        transformed_course.title = course.name
+        transformed_course.grading_standards = []
+        transformed_course.identifier = course.id.to_s
+        transformed_course.grading_standards=[]
+        return transformed_course
+    end   
+    def create_transformed_group(group)
+        transformed_group = CanvasCc::CanvasCC::Models::CanvasModule.new
+        transformed_group.title = group.name
+        transformed_group.identifier = CanvasCc::CC::CCHelper.create_key(transformed_group)
+        return transformed_group
+    end   
+    def create_module_item(content_type,identifier_to_refer_to)
+        module_item = CanvasCc::CanvasCC::Models::ModuleItem.new
+        module_item.content_type=content_type
+        module_item.title = 'in_video_module_item'      
+        module_item.identifier = CanvasCc::CC::CCHelper.create_key(module_item)   
+        module_item.identifierref = identifier_to_refer_to
+        return module_item
+    end   
+    def create_transformed_lecture(lecture)
+        transformed_lecture=CanvasCc::CanvasCC::Models::ModuleItem.new
+        transformed_lecture.identifier = CanvasCc::CC::CCHelper.create_key(transformed_lecture)
         transformed_lecture.title = lecture.name
         transformed_lecture.url = lecture.url
         transformed_lecture.content_type = "ExternalUrl"
         return transformed_lecture
-    end         
+    end   
+    def create_transformed_assessment(quiz_src) #quiz_src could be lecture or stand-alone quiz
+        assessment=CanvasCc::CanvasCC::Models::Assessment.new
+        assessment.quiz_type ='practice_quiz'
+        assessment.items = []
+        assessment.identifier = CanvasCc::CC::CCHelper.create_key(assessment)
+        return assessment 
+    end
+    def create_transformed_question(transformed_question_type)
+        transformed_question = CanvasCc::CanvasCC::Models::Question.create(transformed_question_type)
+        transformed_question.identifier = CanvasCc::CC::CCHelper.create_key(transformed_question)
+        return transformed_question
+    end    
+    def create_transformed_answer(answer)
+        transformed_answer = CanvasCc::CanvasCC::Models::Answer.new
+        transformed_answer.id = CanvasCc::CC::CCHelper.create_key(transformed_answer)
+        transformed_answer.answer_text = extract_inner_html_text(answer.content)
+        transformed_answer.feedback = extract_inner_html_text(answer.explanation)
+        return transformed_answer
+    end  
+    def format_in_video_quiz_body(in_video_quiz,lecture_url)
+        timed_url = format_timed_lecture_url(in_video_quiz,lecture_url)        
+        question = in_video_quiz.question
+        tmp_question_body = "<p><iframe src='#{timed_url}' width='560' height='314' allowfullscreen='allowfullscreen'></iframe>#{question}</p>"
+        return tmp_question_body
+    end   
+    def format_timed_lecture_url(in_video_quiz,lecture_url)
+        tmp = lecture_url.remove('watch?v=')
+        tmp = tmp.gsub(/.com\//,".com\/embed\/")
+        start_time = in_video_quiz.start_time.floor()
+        end_time   = in_video_quiz.start_time.floor()+1
+        tmp +="?start=#{start_time}&end=#{end_time}&autoplay=0mute=0&enablejsapi=0 "
+        return tmp
+    end      
+    def map_SL_quiz_type_to_CC_question_type(type,quiz)        
+        case type
+        when "OCQ"
+            tranformed_question_type="multiple_choice_question"
+        when "MCQ" 
+            tranformed_question_type="multiple_answers_question"
+        when "Free Text Question"
+            tranformed_question_type= quiz.answers[0].content==""? "essay_question" : "fill_in_multiple_blanks_question"
+        end
+        return tranformed_question_type
+    end
+    def extract_inner_html_text(html)
+        innerText =  ActionController::Base.helpers.strip_tags(html)
+        return innerText
+    end 
+     
 end
+
+
+
+# # # //////////////////////////////////
+# transformed_course = CanvasCc::CanvasCC::Models::Course.new
+# course = Course.last
+# transformed_course.title = 'course 125'
+# transformed_course.grading_standards = []
+# transformed_course.identifier = CanvasCc::CC::CCHelper.create_key(transformed_course)
+
+
+# myModule = CanvasCc::CanvasCC::Models::CanvasModule.new
+# myModule.title = 'first module 125'
+# myModule.identifier = CanvasCc::CC::CCHelper.create_key(myModule)
+
+# assessment = CanvasCc::CanvasCC::Models::Assessment.new
+# assessment.quiz_type ='practice_quiz'
+# assessment.items=[]
+# assessment.title = 'my assessmemnt 125'
+# assessment.identifier = CanvasCc::CC::CCHelper.create_key(assessment)
+
+# module_item=CanvasCc::CanvasCC::Models::ModuleItem.new
+# module_item.title = 'my assessment 125b'
+# module_item.content_type='Quizzes::Quiz'
+# module_item.identifierref = assessment.identifier
+# module_item.identifier = CanvasCc::CC::CCHelper.create_key(module_item)
+
+
+# question = CanvasCc::CanvasCC::Models::Question.create('text_only_question')
+# question.identifier='456'
+# question.title='is this a title?'
+# question.material = '<p><iframe src="//www.youtube.com/embed/91a3JA6Fan0?start=6&amp;end=10" width="560" height="314" allowfullscreen="allowfullscreen"></iframe>why</p>'
+
+# answer = CanvasCc::CanvasCC::Models::Answer.new()
+# answer.id='123'
+# answer.feedback='explanation'
+# answer.answer_text='question answer'
+# page = CanvasCc::CanvasCC::Models::Page.new
+# page.identifier = 3
+# page.page_name = 'my page name'
+# page.body = 'text in page'
+# page.workflow_state="active"
+# page.title = 'page title'
+# question.answers << answer
+# assessment.items << question
+# module_item.identifierref = assessment.identifier.to_i
+# myModule.module_items << module_item
+# transformed_course.canvas_modules << myModule
+# transformed_course.assessments<<assessment
+# transformed_course.pages << page
