9e2bac079aabb3829e0520898e9b7e3fd4f0f644
poussy <poussy@novelari.com>
2020-03-10 13:25:43 +0200

    export 1 lecture to FBF

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -816,7 +816,17 @@ class LecturesController < ApplicationController
 
 	# def load_note
 	# end
-
+	def export_lecture_to_feedbackfruit
+		lec_url = @lecture.url
+		teacher_email = @lecture.course.user.email
+		lec_name = @lecture.name
+		export_accomplished = export_to_fbf(lec_url, teacher_email, lec_name) if is_youtube(lec_url)||is_vimeo(lec_url)
+		if export_accomplished
+			render json: {:notice => "Export lecture to feedbackFruit accomplished"}
+		else 
+			render json: {:errors => "Export lecture to feedbackFruit failed"}
+		end
+    end 
 	def lecture_copy
 		id = params[:id] || params[:lecture_id]
 		old_lecture = Lecture.find(id)
@@ -1055,16 +1065,14 @@ class LecturesController < ApplicationController
 	end	
 	
 	def delete_vimeo_video
-		lecture_url_not_used_elsewhere = Lecture.where(:url=>	@lecture_url).count==0
+		lecture_url_not_used_elsewhere = Lecture.where(:url=>@lecture_url).count==0
 		if lecture_url_not_used_elsewhere
 			vid_vimeo_id = 	@lecture_url.split('https://vimeo.com/')[1]
 			delete_video_from_vimeo_account(vid_vimeo_id)
 			delete_video_upload_record(vid_vimeo_id) 
 		end	
 	end	
-	def get_feedbackFruit_access_token
-		return get_fbf_access_token
-    end 		
+		
 private
 	def lecture_params
 		params.require(:lecture).permit(:course_id, :description, :name, :url, :group_id, :appearance_time, :due_date, :duration,
--- app/modules/feedbackfruit_utils.rb
+++ app/modules/feedbackfruit_utils.rb
@@ -1,5 +1,25 @@
 module FeedbackfruitUtils
-  
+    def export_to_fbf(url,teacher_email,title)
+        teacher_email = 'poussy.amr.nileu@gmail.com'
+        #get access token
+        access_token = get_fbf_access_token
+        #get acvitivty group id
+        group_id = get_group_id(access_token)
+        #get media id 
+        media_id = get_media_id(url,access_token)
+        #get video activity id
+        activity_video_id = get_activity_video_id(media_id,title,group_id,access_token)
+        #attach activity id to activity group
+        attachment_accomplished = attach_video_activity_to_group(group_id,activity_video_id,access_token)
+        #register teacher email
+        if attachment_accomplished
+            email_id = register_teacher_email_on_fbf(teacher_email,access_token)
+            #send teacher invitation
+            invitation_accomplished = send_teacher_invitation_on_fbf_video(email_id,group_id,access_token)
+            return true
+        end    
+        return false
+    end     
     def get_fbf_access_token
         query_url =	'https://staging-accounts.feedbackfruits.com/auth/token'
         response = ""
@@ -25,5 +45,120 @@ module FeedbackfruitUtils
         access_token = response['access_token']
         return access_token
     end
+    def get_group_id(access_token)
+        query_url =	'https://staging-api.feedbackfruits.com/v1/activity_groups'
+        response = ""
+        
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
 
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.post(query_url,
+                :headers => { 'Content-Type' => 'application/vnd.api+json','Authorization'=>'Bearer '+access_token } ,
+                :body=>'{"data":{"attributes":{"enrollability":"restricted"},"relationships":{"extension":{"data":{"type":"extensions","id":"video"}}},"type":"activity-groups"}}'         
+            )
+        end	    
+        group_id=response.parsed_response['data']['id']
+       return group_id
+    end    
+    def get_media_id(url,access_token)
+        query_url =	'https://staging-media.feedbackfruits.com'
+        response = ""
+        pp "<<<<<<<<<<<<<<<<<<<<< getting media id >>>>>>>>>>>>>>>>>>>>."
+        pp Time.now.strftime("%d/%m/%Y %H:%M")
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
+
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.post(query_url,
+                :headers => { 'Content-Type' => 'application/x-www-form-urlencoded','Authorization'=>'Bearer '+access_token } ,
+                :body=>{
+                    :url=>url
+                }              
+            )
+        end	 
+        pp "------------------------------------------------------------"
+        pp Time.now.strftime("%d/%m/%Y %H:%M")
+        media_id = response.parsed_response['id']   
+        return media_id
+    end
+    def get_activity_video_id(media_id,title,group_id,access_token)
+        query_url =	'https://staging-api.feedbackfruits.com/v1/engines/multimedia/videos'
+        response = ""
+        
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
+
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.post(query_url,
+                :headers => { 'Content-Type' => 'application/vnd.api+json','Authorization'=>'Bearer '+access_token } ,
+                :body=>'{"data":{"attributes":{"title":"' +title+ '"},' +
+                '"relationships":{"media":{"data":{"type":"media","id":"' +media_id+ '"}},' +
+                '"extension":{"data":{"type":"extensions","id":"video"}},' +
+                '"group":{"data":{"type":"activity-groups","id":"' +group_id + '"}}},"type":"videos"}}'
+            )
+        end	    
+        activity_video_id=response.parsed_response['data']['id']
+       return  activity_video_id
+    end
+    def attach_video_activity_to_group(group_id,activity_video_id,access_token)
+        query_url =	'https://staging-api.feedbackfruits.com/v1/activity_groups/' + group_id
+        response = ""
+        
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
+
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.patch(query_url,
+                :headers => { 'Content-Type' => 'application/vnd.api+json','Authorization'=>'Bearer '+access_token } ,
+                :body=>'{"data":{"id":"' + group_id + '",' +
+                '"attributes":{},' +
+                '"relationships":{"activity":{"data":{"type":"videos","id":"' + activity_video_id + '"}},' +
+                '"extension":{"data":{"type":"extensions","id":"video"}}},"type":"activity-groups"}}'
+            )
+        end	    
+       attach_successful = response.code==200
+       return attach_successful
+    end
+    def register_teacher_email_on_fbf(teacher_email,access_token)
+        query_url =	'https://staging-api.feedbackfruits.com/v1/emails'
+        response = ""
+        
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
+
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.post(query_url,
+                :headers => { 'Content-Type' => 'application/vnd.api+json','Authorization'=>'Bearer '+access_token } ,
+                :body=>'{"data":{"attributes":{"address":"' + teacher_email + '"},"type":"emails"}}'
+            )
+        end	    
+        email_id = response.parsed_response['data']['id']
+       return email_id
+    end
+    def send_teacher_invitation_on_fbf_video(email_id,group_id,access_token)
+        query_url =	'https://staging-api.feedbackfruits.com/v1/invitations'
+        response = ""
+        
+        handler = Proc.new do |exception, attempt_number, total_delay|
+            puts "retreiving acess token from feedback fruit failed. saw a #{exception.class}; retry attempt #{attempt_number}; #{total_delay} seconds have passed."     
+        end
+
+        with_retries(:max_tries => 3, :base_sleep_seconds => 0.5, :max_sleep_seconds => 1.0, :handler => handler, :rescue => [Rack::Timeout::RequestTimeoutException, Timeout::Error, SocketError]) do |attempt_number|
+            response = HTTParty.post(query_url,
+                :headers => { 'Content-Type' => 'application/vnd.api+json','Authorization'=>'Bearer '+access_token } ,
+                :body=>'{"data":{"attributes":{"admin":true,"status":"pending"},"relationships":{"group":{"data":{"type":"activity-groups","id":"' + group_id + '"}},"email":{"data":{"type":"emails","id":"' + email_id + '"}}},"type":"invitations"}}'
+            )
+        end	    
+        invitation_accomplished = response.code==200
+       return invitation_accomplished
+    end
+    def is_youtube(url)
+       return  url.include?('www.youtube.com')
+    end        
   end
