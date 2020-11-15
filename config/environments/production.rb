bcd0b0f618d81ac943e1cb891a2f10e3342293d0
poussy <poussy@novelari.com>
2020-11-15 15:26:25 +0200

    fix port number of sendgrid on all platforms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '465',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
